e32f582d684ee207ebce83e493a1822ce5bd6041
Kuba <j.pikula97@gmail.com>
2023-07-31 14:44:15 +0200

    Added countdown and timer mechanism

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -11,9 +12,20 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5 * 60)
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -27,10 +39,10 @@ title_label.grid(column=1, row=0)
 canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 28, 'bold'))
+timer_text = canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 28, 'bold'))
 canvas.grid(column=1, row=1)
 
-start_button = tkinter.Button(text='Start', highlightthickness=0)
+start_button = tkinter.Button(text='Start', highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = tkinter.Button(text='Reset', highlightthickness=0)
